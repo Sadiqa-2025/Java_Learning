e88bd874ddec0eb2fb033756a07904cbc6e7a142
Sadiqa-2025 <sadiqanishat059@gmail.com>
2025-07-13 23:19:25 +0530

    Even or Odd number

---
+++ src/EvenOrOddNumber.java
@@ -0,0 +1,11 @@
+public class EvenOrOddNumber {
+    public static void main(String[] args) {
+        int num = 11;
+        if(num%2==0){
+            System.out.println("The number is even");
+        }
+        else{
+            System.out.println("The number is odd");
+        }
+    }
+}
