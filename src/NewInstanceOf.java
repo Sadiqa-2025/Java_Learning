5700d6467cc2a7e902456a5fd07b767f1be98aca
Sadiqa-2025 <sadiqanishat059@gmail.com>
2025-07-18 17:21:17 +0530

    InstanceOf and new keyword

---
+++ src/NewInstanceOf.java
@@ -0,0 +1,7 @@
+public class NewInstanceOf {
+    public static void main(String[] args) {
+        NewInstanceOf obj1 = new NewInstanceOf();
+        System.out.println(obj1);
+        System.out.println(obj1 instanceof NewInstanceOf);
+    }
+}
