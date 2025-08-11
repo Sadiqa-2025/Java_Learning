4fcf38fd653afe80e3e6490f06910e05e226f9b8
Sadiqa-2025 <sadiqanishat059@gmail.com>
2025-08-11 22:08:21 +0530

    Sub string

---
+++ src/StringPrograms/String023.java
@@ -0,0 +1,25 @@
+package StringPrograms;
+/*
+"input: String str = ""HelloWrold"";
+output = ""World"";"
+ */
+public class String023 {
+
+    public static void main(String[] args) {
+        String s1 = "HelloWorld";
+        int count =0;
+        for(int i =0; i<s1.length(); i++){
+            if(s1.charAt(i)>='A' && s1.charAt(i)<='Z'){
+                count++;
+                if(count ==2){
+                    System.out.println(s1.charAt(i));
+                    System.out.println(s1.substring(i));
+                    break;
+                }
+            }
+
+
+        }
+
+    }
+}
