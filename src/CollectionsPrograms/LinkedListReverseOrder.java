928385d993f07a63d4acd0a9ae279ab324aaa667
Sadiqa-2025 <sadiqanishat059@gmail.com>
2025-08-25 17:55:45 +0530

    25-Aug

---
+++ src/CollectionsPrograms/LinkedListReverseOrder.java
@@ -0,0 +1,29 @@
+package CollectionsPrograms;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+"Create a LinkedList of integers and:
+1. Add elements
+2. Remove the first and last element
+3. Print the list in reverse order"
+ */
+public class LinkedListReverseOrder {
+    public static void main(String[] args) {
+        LinkedList<Integer> num = new LinkedList<>();
+        num.add(10);
+        num.add(12);
+        num.add(15);
+        num.add(17);
+        num.add(20);
+        num.add(8);
+        System.out.println(num);
+        num.removeFirst();
+        num.removeLast();
+        System.out.println(num);
+        for(int i=num.size()-1; i>=0; i--){
+            System.out.println(num.get(i));
+        }
+    }
+}
