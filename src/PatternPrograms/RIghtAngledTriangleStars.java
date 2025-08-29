a0851a55e609c2e691a73a75f6f1272e776c0cf1
Sadiqa-2025 <sadiqanishat059@gmail.com>
2025-08-29 18:23:40 +0530

    29-Aug

---
+++ src/PatternPrograms/RIghtAngledTriangleStars.java
@@ -0,0 +1,13 @@
+package PatternPrograms;
+
+public class RIghtAngledTriangleStars {
+
+    public static void main(String[] args) {
+        for(int rows=1; rows<=5; rows++){
+            for(int star=1; star<=rows ; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
